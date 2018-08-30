90e77ca708abb5db92953462727e014ff7c1636c
Vadhil Amadiz <vamadiz@gmail.com>
2018-08-30 15:35:42 -0400

    initial

--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
 /*
  * Create a list that holds all of your cards
  */
- let cardsArray = [
+ let cardsList = [
              'fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt',
              'fa-cube', 'fa-anchor', 'fa-leaf', 'fa-bicycle',
              'fa-diamond', 'fa-bomb', 'fa-leaf', 'fa-bomb',
              'fa-bolt', 'fa-bicycle', 'fa-paper-plane-o', 'fa-cube'
  ];
-
 
 /*
  * Display the cards on the page
@@ -15,6 +14,7 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -36,9 +36,14 @@ function shuffle(array) {
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+
  *  - if the list already has another card, check to see if the two cards match
+
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
